f8795e8425810c90816bf8c1de93b0d3b399ad8f
Scott Doggett <20sjd7@queensu.ca>
2025-10-23 19:43:28 -0700

    Added metadata to load image from uploadthing for preview.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,30 @@ export const metadata: Metadata = {
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
   },
+
+  openGraph: {
+    title: "Ceezaa - Your Digital Passport to Hostel Adventures",
+    description:
+      "Every hostel stay should feel personal. With Ceezaa, your stay adapts to you — your vibe, your tastes, and your rhythm.",
+    url: "https://ceezaa.com",
+    siteName: "Ceezaa",
+    images: [
+      {
+        url: "https://bl974d7s47.ufs.sh/f/eiqM2oDvPqg1gwu44a0X6UpLkW4Zv2BarPy9lFbSdHIsonzi", // 👈 UploadThing image URL
+        width: 1200,
+        height: 630,
+        alt: "Ceezaa hero preview",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ceezaa - Your Digital Passport to Hostel Adventures",
+    description:
+      "Every hostel stay should feel personal. With Ceezaa, your stay adapts to you — your vibe, your tastes, and your rhythm.",
+    images: ["https://bl974d7s47.ufs.sh/f/eiqM2oDvPqg1gwu44a0X6UpLkW4Zv2BarPy9lFbSdHIsonzi"], // 👈 same UploadThing URL
+  },
 };
 
 export default function RootLayout({
@@ -30,8 +54,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${manrope.variable} antialiased`}>
         <Header />
-              {children}
-              <Footer />
+          {children}
+        <Footer />
       </body>
     </html>
   );
